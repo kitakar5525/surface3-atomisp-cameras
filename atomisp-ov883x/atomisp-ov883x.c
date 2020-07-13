91cd4b7a0c69d32ebc3329205f52e8fd115e7d47
Tsuchiya Yuto <kitakar@gmail.com>
2020-07-14 01:22:50 +0900

    atomisp-ov883x: (port-to-v4.2) drop enum_mbus_fmt in favor of enum_mbus_code
    In accordance with commit ebcff5fce6b1 ("[media] v4l2: replace enum_mbus_fmt
    by enum_mbus_code") added on v4.2-rc1.
    
    This commit drops enum_mbus_fmt video op. It's replaced with enum_mbus_code
    upstream, but this driver already has the pad op. So, just removing the
    video op code.
    
    Fixes the errors like the following:
    
        atomisp-ov883x/atomisp-ov883x.c:1437:11: warning: ‘enum v4l2_mbus_pixelcode’ declared inside parameter list will not be visible outside of this definition or declaration
         1437 |      enum v4l2_mbus_pixelcode *code)
              |           ^~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- atomisp-ov883x/atomisp-ov883x.c
+++ atomisp-ov883x/atomisp-ov883x.c
@@ -1340,13 +1340,6 @@ static int ov8830_s_stream(struct v4l2_subdev *sd, int enable)
 	return 0;
 }
 
-static int ov8830_enum_mbus_fmt(struct v4l2_subdev *sd, unsigned int index,
-				 enum v4l2_mbus_pixelcode *code)
-{
-	*code = MEDIA_BUS_FMT_SBGGR10_1X10;
-	return 0;
-}
-
 static int ov8830_s_config(struct v4l2_subdev *sd,
 			    int irq, void *pdata)
 {
@@ -1670,7 +1663,6 @@ static int ov8830_g_skip_frames(struct v4l2_subdev *sd, u32 *frames)
 
 static const struct v4l2_subdev_video_ops ov8830_video_ops = {
 	.s_stream = ov8830_s_stream,
-	.enum_mbus_fmt = ov8830_enum_mbus_fmt,
 	.try_mbus_fmt = ov8830_try_mbus_fmt,
 	.g_mbus_fmt = ov8830_g_mbus_fmt,
 	.s_mbus_fmt = ov8830_s_mbus_fmt,
