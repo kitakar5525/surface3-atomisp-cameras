699016c3c1a3c954ea781f0320855133489d136a
Tsuchiya Yuto <kitakar@gmail.com>
2021-08-23 21:40:01 +0900

    ov883x: drop enum_mbus_fmt
    enum_mbus_fmt() is deprecated and these sensor drivers already support
    enum_mbus_code(). So, drop it.
    
    Note that atomisp driver already supports (and actually prefers over
    enum_mbus_fmt()) enum_mbus_code().
    
    In atomisp_ioctl.c:
    	rval = v4l2_subdev_call(isp->inputs[asd->input_curr].camera, pad,
    				enum_mbus_code, NULL, &code);
    	if (rval == -ENOIOCTLCMD) {
    		dev_warn(isp->dev, "enum_mbus_code pad op not supported. Please fix your sensor driver!\n");
    		rval = v4l2_subdev_call(isp->inputs[asd->input_curr].camera,
    					video, enum_mbus_fmt, 0, &code.code);
    	}
    
    Signed-off-by: Tsuchiya Yuto <kitakar@gmail.com>

--- atomisp-ov883x/atomisp-ov883x.c
+++ atomisp-ov883x/atomisp-ov883x.c
@@ -1228,14 +1228,6 @@ static int ov8830_s_stream(struct v4l2_subdev *sd, int enable)
 	return 0;
 }
 
-static int ov8830_enum_mbus_fmt(struct v4l2_subdev *sd, unsigned int index,
-				 u32 *code)
-{
-	pr_info("%s() called\n", __func__);
-	*code = MEDIA_BUS_FMT_SBGGR10_1X10;
-	return 0;
-}
-
 static int ov8830_s_config(struct v4l2_subdev *sd,
 			    int irq, void *platform_data)
 {
@@ -1519,7 +1511,6 @@ static int ov8830_g_skip_frames(struct v4l2_subdev *sd, u32 *frames)
 
 static const struct v4l2_subdev_video_ops ov8830_video_ops = {
 	.s_stream = ov8830_s_stream,
-	.enum_mbus_fmt = ov8830_enum_mbus_fmt,
 	.try_mbus_fmt = ov8830_try_mbus_fmt,
 	.g_mbus_fmt = ov8830_g_mbus_fmt,
 	.s_mbus_fmt = ov8830_s_mbus_fmt,
